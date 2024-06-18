cc5f8bf5a978264d6c62fb5242d11dd7d466b6e1
NikitaKirillov0205 <nikitakirillov0205@gmail.com>
2024-06-18 15:08:04 +0300

    First version of the website

---
+++ index.js
@@ -0,0 +1,21 @@
+const first = document.getElementById("scrolling-txt")
+const second = document.getElementById("scrolling-txt2")
+const container = document.getElementById("container")
+const rect = container.getBoundingClientRect()
+
+const animate = (element,position) => {
+    element.style.transform = `translateX(${position}px)`
+} 
+
+document.addEventListener('scroll', function(e) {
+    lastKnownScrollPosition = window.scrollY - 500;
+      
+     window.requestAnimationFrame(function() {
+       
+        animate(first,lastKnownScrollPosition*-.9)
+        animate(second,(lastKnownScrollPosition-300)*.9)
+      });
+  });
+
+
+
